7adc1b50d9385767b9c8601f1ed4cef9a9ead8f4
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2025-09-19 02:16:59 +0300

    Return to the base of cmake again

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,12 @@
 #include <iostream>
 
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include <SDL2_mixer/SDL_mixer.h>
-#include <SDL2_ttf/SDL_ttf.h>
-
-#include <glm/glm.hpp>
-
-#include <sol/sol.hpp>
-
-#include <imgui.h>
 
 int main () 
 {
-    std::cout << "Hello World!" << std::endl;
-    
     SDL_Init(SDL_INIT_VIDEO);
 
-    glm::vec2 velocity = glm::vec2(5.0, -2.5);
-    velocity = glm::normalize(velocity);
-
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
-    lua.script("print('Hello from Lua via sol2!')");
+    std::cout << "Hello World\n";
     
     SDL_Quit();
     return 0;
